6707e14aaedd34e1d60b0754932ad0c29819b0b4
Shrikant2603 <93378209+Shrikant2603@users.noreply.github.com>
2023-01-14 00:48:21 +0530

    completed the checkoutproduct

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import Subtotal from './Subtotal'
 import "./Checkout.css"
+import { useStateValue } from "./StateProvider";
+import CheckoutProduct from './CheckoutProduct'
 
 function Checkout() {
+  const [{ basket, user }, dispatch] = useStateValue();
   return (
     <div className='checkout'>
         <div className="checkout__left">
           <div className="checkout__left">
             <h2 className='checkout__title'>Shopping cart</h2>
+            {basket.map(item => (
+            <CheckoutProduct
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+            />
+          ))}
           </div>
         </div>
         <div className="checkout__right">
